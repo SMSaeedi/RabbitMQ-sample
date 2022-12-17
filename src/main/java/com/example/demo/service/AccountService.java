fb9a7f3d7f3498d812d2d6b744935d2d64079e4c
Mahsa <mahsa.saeedy@gmail.com>
2022-12-17 15:15:29 +0330

    add mock messaging config

--- src/main/java/com/example/demo/service/AccountService.java
+++ src/main/java/com/example/demo/service/AccountService.java
@@ -1,7 +1,13 @@
 package com.example.demo.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AccountService {
+    private final ObjectMapper mapper;
+
+    public AccountService(ObjectMapper mapper) {
+        this.mapper = mapper;
+    }
 }
